76b6dd051974fe56700ca2c0b61aa4cca4e6d94e
Spark <79936503+spa5k@users.noreply.github.com>
2024-05-16 12:01:09 +0000

    chore: Update npm dependency to pnpm@9.1.1 and add lazy loading for images
    This commit updates the npm dependency in the package.json file to pnpm@9.1.1. Additionally, it adds lazy loading for images in the next.config.js file using the remotePatterns configuration. These changes improve the performance and user experience of the application.

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   output: "standalone",
+  // image from unsplash.it
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "unsplash.it",
+      },
+    ],
+  },
 };
