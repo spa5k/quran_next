71f3b40b56e54e44b55ae6ed83ef7b0123ff9e6f
Spark <79936503+spa5k@users.noreply.github.com>
2024-05-19 04:22:08 +0000

    feat: Add findPort utility function for Electron app and improve SurahCard component
    The `findPort` utility function is added to the Electron app to find an available port within a specified range. This function checks if a port is already stored in the cache and returns it if found. Otherwise, it iterates through the port range and checks if each port is available by making a request to `http://localhost:{port}/health`. If an available port is found, it is stored in the cache for future use.
    
    In the SurahCard component, a link is added to navigate to the individual surah page. This improves the user experience by allowing users to easily access detailed information about a specific surah.
    
    These changes enhance the functionality and user experience of the application.

--- apps/web/src/features/quran/SurahCard.tsx
+++ apps/web/src/features/quran/SurahCard.tsx
@@ -2,38 +2,41 @@ import { Button } from "@/src/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/src/components/ui/card";
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/src/components/ui/tooltip";
 import { BookmarkIcon } from "lucide-react";
+import Link from "next/link";
 import { Chapter } from "./types";
 
 const QuranCard = ({ chapter, juzStart, juzEnd }: { chapter: Chapter; juzStart: number; juzEnd: number }) => {
   const juzRange = juzStart !== juzEnd && juzEnd ? `Juz ${juzStart}-${juzEnd}` : `Juz ${juzStart}`;
   return (
-    <Card className="h-48 flex flex-col justify-between">
-      <CardHeader className="flex flex-row">
-        <div className="gap-2 flex flex-col">
-          <CardTitle>
-            <TooltipProvider>
-              <Tooltip>
-                <TooltipTrigger>{chapter.name}</TooltipTrigger>
-                <TooltipContent>
-                  <p className="font-arabic">{chapter.verses.length} Verses - {chapter.revelation}</p>
-                </TooltipContent>
-              </Tooltip>
-            </TooltipProvider>
-          </CardTitle>
-          <CardDescription>
-            {juzRange}, Surah {chapter.chapter}
-          </CardDescription>
-        </div>
-        <Button className="ml-auto" size="sm" variant="ghost">
-          <BookmarkIcon />
-          <span className="sr-only">Bookmark</span>
-        </Button>
-      </CardHeader>
-      <CardContent className="flex justify-between w-full">
-        <p className="">{chapter.englishname}</p>
-        <p className="font-arabic text-right">{chapter.arabicname}</p>
-      </CardContent>
-    </Card>
+    <Link href={`/surah/${chapter.chapter}`}>
+      <Card className="h-48 flex flex-col justify-between">
+        <CardHeader className="flex flex-row">
+          <div className="gap-2 flex flex-col">
+            <CardTitle>
+              <TooltipProvider>
+                <Tooltip>
+                  <TooltipTrigger>{chapter.name}</TooltipTrigger>
+                  <TooltipContent>
+                    <p className="font-arabic">{chapter.verses.length} Verses - {chapter.revelation}</p>
+                  </TooltipContent>
+                </Tooltip>
+              </TooltipProvider>
+            </CardTitle>
+            <CardDescription>
+              {juzRange}, Surah {chapter.chapter}
+            </CardDescription>
+          </div>
+          <Button className="ml-auto" size="sm" variant="ghost">
+            <BookmarkIcon />
+            <span className="sr-only">Bookmark</span>
+          </Button>
+        </CardHeader>
+        <CardContent className="flex justify-between w-full">
+          <p className="">{chapter.englishname}</p>
+          <p className="font-arabic text-right">{chapter.arabicname}</p>
+        </CardContent>
+      </Card>
+    </Link>
   );
 };
 
